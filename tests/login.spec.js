db5179ece89e00aa1261c6055075dfb68e8ab762
Rukpong.R <rukpong.r@arcadiaapm.com>
2025-08-07 11:11:44 +0700

    clean file structure and edit util functions

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,9 +1,7 @@
 import { test, expect } from '../fixtures/test-fixtures.js';
-import { getMessage, loadLanguage } from '../utils/localeLoader.js';
 import { envConfig } from '../utils/envLoader.js';
+import { lang, messages } from '../utils/localeLoader.js';
 
-const lang = process.env.TEST_LANG || 'en';
-loadLanguage(lang);
 
 test.describe(`Login Functionality (${lang})`, () => {
   test.beforeEach(async ({ loginPage }) => {
@@ -14,7 +12,7 @@ test.describe(`Login Functionality (${lang})`, () => {
     const user = testData.users.standardUser;
     await loginPage.login(user.username, user.password);
     await expect(homePage.title).toBeVisible();
-    await expect(homePage.title).toHaveText(getMessage('homePage.productTitle'));
+    await expect(homePage.title).toHaveText(messages.homePage.productTitle);
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
     console.log('Login successful for standard user');
   });
@@ -23,7 +21,7 @@ test.describe(`Login Functionality (${lang})`, () => {
     const user = testData.users.lockedOutUser;
     console.log('Running test: should show an error message for locked out user');
     await loginPage.login(user.username, user.password);
-    await expect(loginPage.errorMessage).toHaveText(getMessage('loginPage.lockedOutUserError'));
+    await expect(loginPage.errorMessage).toHaveText(messages.loginPage.lockedOutUserError);
     console.log('Error message verified for locked out user.');
   });
 
@@ -31,7 +29,7 @@ test.describe(`Login Functionality (${lang})`, () => {
     const user = testData.users.invalidUser;
     console.log('Running test: should show an error message for invalid credentials');
     await loginPage.login(user.username, user.password);
-    await expect(loginPage.errorMessage).toHaveText(getMessage('loginPage.invalidCredentialsError'))
+    await expect(loginPage.errorMessage).toHaveText(messages.loginPage.invalidCredentialsError);
     console.log('Error message verified for invalid credentials.');
   });
 
